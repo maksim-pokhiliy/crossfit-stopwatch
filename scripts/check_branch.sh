8e76d5b472e416ab935a54222426f9339674ecae
Maksim Pokhiliy <maksim.pokhiliy@gmail.com>
2025-01-09 12:57:59 +0000

    chore: allow vercel deploy only from the master branch

--- scripts/check_branch.sh
+++ scripts/check_branch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ "$(git rev-parse --abbrev-ref HEAD)" != "dev" ]; then
-  echo "Deployment is allowed only from the 'dev' branch."
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]; then
+  echo "Deployment is allowed only from the 'master' branch."
   exit 1
 fi
+
