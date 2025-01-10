88ec7fded887e1463b2f9e96db579830aaa0f9bf
Maksim Pokhiliy <maksim.pokhiliy@gmail.com>
2025-01-10 21:41:46 +0000

    chore: basic tests setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,4 +21,14 @@ export default defineConfig({
     sourcemap: true,
   },
   publicDir: "public",
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    css: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+    },
+  },
 });
